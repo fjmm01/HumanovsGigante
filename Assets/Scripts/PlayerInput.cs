634819bc99df0bed3449457db556cfc57464e5b2
fjmm01 <francisco.murillo@escuelaestech.es>
2022-10-07 10:25:44 +0200

    commit 07 10 2022 10 25

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,16 +4,122 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    
-    void Start()
+    [SerializeField] float maxSpeed = 10;
+    [SerializeField] float acceleration = 35;
+    [SerializeField] float jumpSpeed = 8;
+    [SerializeField] float jumpDuration;
+    [SerializeField] bool enableDoubleJump = true;
+    [SerializeField] bool wallHitDoubleJumpOverride = true;
+
+    //internal checks
+    bool canDoubleJump = true;
+    float jumpDur;
+    bool jumpKeyDown = false;
+    bool canVariableJump = false;
+    Rigidbody2D rigidbody2D;
+    void Start() 
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal < -0.1f)
+        {
+            if (rigidbody2D.velocity.x > -this.maxSpeed)
+            {
+                rigidbody2D.AddForce(new Vector2(-this.acceleration, 0.0f));
+            }
+            else
+            {
+                rigidbody2D.velocity = new Vector2(-this.maxSpeed, rigidbody2D.velocity.y);
+            }
+        }
+        else if(horizontal > 0.1f)
+        {
+            if (rigidbody2D.velocity.x < this.maxSpeed)
+            {
+                rigidbody2D.AddForce(new Vector2(this.acceleration, 0.0f));
+            }
+            else
+            {
+                rigidbody2D.velocity = new Vector2(this.maxSpeed, rigidbody2D.velocity.y);
+            }
+        }
+
+        bool onTheGround = isOnGround();
+        float vertical = Input.GetAxis("Vertical");
+
+        if(onTheGround)
+        {
+            canDoubleJump = true;
+        }
+        if (vertical > 0.1f)
+        {
+            if(!jumpKeyDown)
+            {
+                jumpKeyDown = true;
+                if(onTheGround || (canDoubleJump && enableDoubleJump) || wallHitDoubleJumpOverride)
+                {
+                    bool wallHit = false;
+                    int wallHitDirection = 0;
+
+                    bool leftWallHit = isOnWallLeft();
+                    bool rightWallHit = isOnWallRight();
+
+                    if(horizontal != 0)
+                    {
+                        if(leftWallHit)
+                        {
+                            wallHit = true;
+                            wallHitDirection = 1;
+                        }
+                        else if(rightWallHit)
+                        {
+                            wallHit = true;
+                            wallHitDirection = -1;
+                        }
+                    }
+                    if(!wallHit)
+                    {
+                        if(onTheGround || (canDoubleJump && enableDoubleJump))
+                        {
+                            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, this.jumpSpeed);
+                            jumpDur = 0.0f;
+                            canVariableJump = true;
+                        }
+                    }
+                    else
+                    {
+                        rigidbody2D.velocity = new Vector2(this.jumpSpeed * wallHitDirection, this.jumpSpeed);
+
+                        jumpDur = 0.0f;
+                        canVariableJump = true;
+                    }
+
+                    if(!onTheGround && !wallHit)
+                    {
+                        canDoubleJump = false;
+                    }
+                }
+            }
+            else if(canVariableJump)
+            {
+                jumpDur += Time.deltaTime;
+                if(jumpDur < jumpDuration /1000)
+                {
+                    rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, this.jumpSpeed);
+                }
+            }
+        }
+        else
+        {
+            jumpKeyDown = false;
+            canVariableJump = false;
+        }
     }
 
     private bool isOnGround()
@@ -30,9 +136,45 @@ public class PlayerInput : MonoBehaviour
 
     private bool isOnWallLeft()
     {
+        bool retVal = false;
         float lengthToSearch = 0.1f;
-        float collidetThreshold = 0.01f;
+        float colliderThreshold = 0.01f;
 
-        Vector2 lineStart = 
+        Vector2 lineStart = new Vector2(this.transform.position.x - this.GetComponentInChildren<SpriteRenderer>().bounds.extents.x - colliderThreshold, this.transform.position.y);
+        Vector2 vectorToSearch = new Vector2(lineStart.x - lengthToSearch, this.transform.position.y);
+
+        RaycastHit2D hitLeft = Physics2D.Linecast(lineStart, vectorToSearch);
+
+        retVal = hitLeft;
+        if(retVal)
+        {
+            if(hitLeft.collider.GetComponent<NoSlideJump>())
+            {
+                retVal = false;
+            }
+        }
+        return retVal;
+    }
+
+    private bool isOnWallRight()
+    {
+        bool retVal = false;
+        float lengthToSearch = 0.1f;
+        float colliderThreshold = 0.01f;
+
+        Vector2 lineStart = new Vector2(this.transform.position.x + this.GetComponentInChildren<SpriteRenderer>().bounds.extents.x + colliderThreshold, this.transform.position.y);
+        Vector2 vectorToSearch = new Vector2(lineStart.x + lengthToSearch, this.transform.position.y);
+
+        RaycastHit2D hitRight = Physics2D.Linecast(lineStart, vectorToSearch);
+
+        retVal = hitRight;
+        if (retVal)
+        {
+            if (hitRight.collider.GetComponent<NoSlideJump>())
+            {
+                retVal = false;
+            }
+        }
+        return retVal;
     }
 }
