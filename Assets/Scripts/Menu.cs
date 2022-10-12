6c8fe928915755c2255ef0a2dcd319d788ea5e05
fjmm01 <francisco.murillo@escuelaestech.es>
2022-10-12 22:41:46 +0200

    commit 12 10 2022 22 40

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
