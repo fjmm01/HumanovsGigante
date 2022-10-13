357e65da43f98be446bd7dbfdbb6651a58f93361
fjmm01 <francisco.murillo@escuelaestech.es>
2022-10-13 11:04:57 +0200

    commit 13 10 2022
    UI funcional con selección de niveles

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,23 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     
-    void Start()
-    {
-        
-    }
-
-    
-    void Update()
-    {
-        
-    }
-
+   
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
     }
 }
