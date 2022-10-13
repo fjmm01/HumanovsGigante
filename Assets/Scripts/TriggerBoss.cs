3e5bffecc6620254c301f756513011f7ae7be0df
fjmm01 <francisco.murillo@escuelaestech.es>
2022-10-14 01:45:01 +0200

    commit 14 10 2022  1 44

--- Assets/Scripts/TriggerBoss.cs
+++ Assets/Scripts/TriggerBoss.cs
@@ -7,12 +7,14 @@ public class TriggerBoss : MonoBehaviour
     [SerializeField] BoxCollider2D trigger;
     [SerializeField] GameObject boss;
     [SerializeField] float timer;
+    [SerializeField] GameObject tileMapPinchos;
    
 
     void Start()
     {
         boss.SetActive(false);
         timer = 0;
+        tileMapPinchos.SetActive(false);
     }
 
     
@@ -29,18 +31,19 @@ public class TriggerBoss : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision == null)
+        if (collision == null)
         {
             return;
         }
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             Debug.Log("You enter to the trap");
             boss.SetActive(true);
+            tileMapPinchos.SetActive(true);
         }
     }
 
-    
+
 }
